73b4cbaaa8c66c8ded95f91c5965220b03728522
divyanshu1234 <divyanshu.pundir@gmail.com>
2017-11-03 00:47:55 +0530

    refactor: Variable name changes

--- main.py
+++ main.py
@@ -14,16 +14,16 @@ helper.trans_data['R'] = input_dataframe[['rot_x', 'rot_y', 'rot_z']].as_matrix(
 helper.trans_data['I'] = input_dataframe['light_intensity'] / input_dataframe['light_intensity'].max()
 time = input_dataframe['time']
 
-xyz_gen1 = input_dataframe[['h', 's', 'v']].as_matrix()
-xyz_gen1[:, 1] = xyz_gen1[:, 1] * 360
-xyz_gen1[:, 2] = xyz_gen1[:, 2] * 360
+xyz_gen0 = input_dataframe[['h', 's', 'v']].as_matrix()
+xyz_gen0[:, 1] = xyz_gen0[:, 1] * 360
+xyz_gen0[:, 2] = xyz_gen0[:, 2] * 360
 
-genome_gen1_org1 = input("Genome org1 (AaRrIi): ")
-genome_gen1_org2 = input("Genome org2 (AaRrIi): ")
+genome_gen0_org1 = input("Genome org1 (AaRrIi): ")
+genome_gen0_org2 = input("Genome org2 (AaRrIi): ")
 max_gen = int(input("Max Generation: "))
 
-org1 = organism.Organism(xyz=xyz_gen1, genome=genome_gen1_org1)
-org2 = organism.Organism(xyz=xyz_gen1, genome=genome_gen1_org2)
+org1 = organism.Organism(xyz=xyz_gen0, genome=genome_gen0_org1)
+org2 = organism.Organism(xyz=xyz_gen0, genome=genome_gen0_org2)
 
 gen1 = organism.Generation(org1=org1, org2=org2, index=0, time=time)
 
